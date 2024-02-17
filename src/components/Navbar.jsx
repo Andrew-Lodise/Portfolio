75313071054b1e1c8b63032f6986b4da5f964498
Andrew-Lodise <AL964440@wcupa.edu>
2024-02-16 19:19:44 -0500

    navbar is presentable

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,39 @@
-import React from 'react'
-//import FaBars from "react-icons/fa"
-import { FaGithub, FaTwitter, FaBars } from 'react-icons/fa';
+import React, { useState } from 'react'
+import{ FaBars } from 'react-icons/fa';
 
 
 const Navbar = () => {
+  // state for FaBars color change to work
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-gray-800 text-white">
-        {/* logo */}
-        <div className="flex justify-center h-[60px] w-[60px] items-center bg-gray-900 text-white font-extrabold ">
-          Logo
-        </div>
+    <div>
+      <div className="fixed w-[100%] h-[80px] flex justify-between p-4 items-center bg-gray-800
+      text-white">
+          {/* logo */}
+          <div className="flex h-[60px] w-[100px] ">
+            <div className='font-greatVibes text-[50px] text-blue-300'>
+            AL
+            </div>
+          </div>
 
-        {/* menu */}
-        <div>
-          <ul className='hidden md:flex p-1 bg-gray-900'>
-            <li className='px-2 bg-blue-800 my-4 mx-6 cursor-pointer'>Projects</li>
-            <li className='px-2 bg-blue-800 my-4 mx-6 cursor-pointer'>Timeline</li>
-            <li className='px-2 bg-blue-800 my-4 mx-6 cursor-pointer'>Skills</li>
-            <li className='px-2 bg-blue-800 my-4 mx-6 cursor-pointer'>About</li>
-            <li className='px-2 bg-blue-800 my-4 mx-6 cursor-pointer'>Contact</li>
-          </ul>
-        </div>
+          {/* menu */}
+          <div>
+            <ul className='hidden md:flex'>
+              <li className='py-2 px-5 m-2 rounded-full border-2 border-white text-white bg-gray-900 cursor-pointer hover:border-blue-300 hover:text-blue-300'>Contact</li>
+              <li className='py-2 px-5 m-2 rounded-full border-2 border-white text-white bg-gray-900 cursor-pointer hover:border-blue-300 hover:text-blue-300'>Projects</li>
+              <li className='py-2 px-5 m-2 rounded-full border-2 border-white text-white bg-gray-900 cursor-pointer hover:border-blue-300 hover:text-blue-300'>Timeline</li>
+              <li className='py-2 px-5 m-2 rounded-full border-2 border-white text-white bg-gray-900 cursor-pointer hover:border-blue-300 hover:text-blue-300'>Skills</li>
+              <li className='py-2 px-5 m-2 rounded-full border-2 border-white text-white bg-gray-900 cursor-pointer hover:border-blue-300 hover:text-blue-300'>About</li>
+            </ul>
+          </div>
 
-        <div className='md:hidden'>
-        <FaBars />
-        </div>
-
-
+          <div className='md:hidden cursor-pointer border-2 p-2 border-white rounded hover:border-blue-300'
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}>
+          <FaBars size={30} color={isHovered ? "#98c4fc" : "white"}/>
+          </div>
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/**<link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet"> */
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        greatVibes: ['"Great Vibes"', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 }
