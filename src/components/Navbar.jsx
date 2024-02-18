49ea0d734c2d9fbc13a7053472613849c5d51903
Andrew-Lodise <AL964440@wcupa.edu>
2024-02-17 20:14:46 -0500

    sidebar implemented

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import{FaTimes} from 'react-icons/fa';
 import { FaBars } from "react-icons/fa6";
 import { RxCross2 } from "react-icons/rx";
-
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { IoDocumentTextOutline } from "react-icons/io5";
 
 const Navbar = () => {
   // state for FaBars color change to work
@@ -12,7 +13,7 @@ const Navbar = () => {
 
   return (
     <div>
-      {/** top navbar */}
+      {/** navbar */}
       <div className="fixed w-[100%] h-[80px] flex justify-between items-center p-4 bg-gray-900 text-white">
         {/* logo */}
         <div className="flex h-[60px] w-[100px] ">
@@ -38,19 +39,35 @@ const Navbar = () => {
               onMouseLeave={() => setIsHovered(false)}
               onClick={handleBarClick}>
         {!miniMenu ? <FaBars size={30} color={isHovered ? "#98c4fc" : "white"}/> : <RxCross2 size={30} color={isHovered ? "#98c4fc" : "white"}/>}
-        
         </div>
 
         {/* mobile menu */}
-        <ul className={!miniMenu ? 'hidden' : 'absolute top-0 left-0 w-full h-screen flex flex-col items-center justify-center bg-gray-900'}>
+        <ul className={!miniMenu ? 'hidden' : 'absolute top-0 left-0 w-full h-screen flex flex-col items-center justify-center bg-gray-900 duration-700'}>
           <li className='my-4 p-2 text-4xl border-2 cursor-pointer border-white rounded-lg hover:border-blue-300 hover:text-blue-300 duration-200'>Projects</li>
           <li className='my-4 p-2 text-4xl border-2 cursor-pointer border-white rounded-lg hover:border-blue-300 hover:text-blue-300 duration-200'>Timeline</li>
           <li className='my-4 p-2 text-4xl border-2 cursor-pointer border-white rounded-lg hover:border-blue-300 hover:text-blue-300 duration-200'>Skills</li>
           <li className='my-4 p-2 text-4xl border-2 cursor-pointer border-white rounded-lg hover:border-blue-300 hover:text-blue-300 duration-200'>About</li>
           <li className='my-4 p-2 text-4xl border-2 cursor-pointer border-white rounded-lg hover:border-blue-300 hover:text-blue-300 duration-200'>Contact</li>
         </ul>
+
+        {/* sidebar */}
+        <div className={miniMenu ? 'hidden' : 'fixed top-[20%] flex flex-col left-0 text-3xl'}>
+          <ul>
+            <div className='flex items-center justify-between w-[200px] ml-[-150px] hover:ml-[-40px] my-2 border-2 border-white hover:border-blue-300 px-2 rounded-full hover:text-blue-300 pl-10 duration-200'>
+              <a href="/">Github</a>
+              <FaGithub/>
+            </div>
+            <li className='flex items-center justify-between w-[230px] ml-[-180px] hover:ml-[-40px] my-2 border-2 border-white hover:border-blue-300 px-2 rounded-full hover:text-blue-300 pl-10 duration-200'>
+              <a href="/" >Linkedin</a>
+              <FaLinkedin/>
+            </li>
+            <li className='flex items-center justify-between w-[230px] ml-[-180px] hover:ml-[-40px] my-2 border-2 border-white hover:border-blue-300 px-2 rounded-full hover:text-blue-300 pl-10 duration-200'>
+              <a href="/" >Resume</a>
+              <IoDocumentTextOutline />
+            </li>
+          </ul>
+        </div>
       </div>
-      
     </div>
   )
 }
