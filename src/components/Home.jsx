f1cad68f41b6f1f943c418fd621ec88f37f133e1
Andrew-Lodise <AL964440@wcupa.edu>
2024-02-19 22:16:17 -0500

    homepage is fine

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,14 +4,19 @@ const Home = () => {
   return (
     <div name='home' className=' w-full h-screen bg-gray-800'>
       {/** container */}
-      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full pt-[100px]'>
+      <div className='mx-auto px-8 flex flex-col justify-center h-full pt-[100px]'>
         <div className='flex flex-col items-center h-full'>
           { /* Andrew Lodise */}
-          <h1 className='top-[110px] text-[80px] font-greatVibes text-blue-300'>Andrew Lodise</h1>
+          <div className=' flex felx-col w-screen items-left'>
+            <h1 className='text-[90px] ml-[50px] mb-[-40px] font-greatVibes text-blue-300'>
+              Andrew Lodise
+            </h1>
+          </div>
+          
           { /* about me section */}
-          <div className='flex flex-col top-[200px] items-left mx-[80px]'>
-            <p className='text-2xl text-white font-bold'> Software Developer</p>
-            <p className='text-xl text-white'>Fill in later ... I am a dedicated and driven computer science student with experience
+          <div className='flex flex-col top-[200px] items-left ml-[30px] mr-[80px]'>
+            <p className='text-2xl md:text-4xl text-white font-bold p-1'> Software Developer</p>
+            <p className='text-xl text-white lg:pr-[300px]'>Fill in later ... I am a dedicated and driven computer science student with experience
             in developing and implementing successful projects. My education includes a Bachelor of 
             Science in Computer Science from West Chester University, where I gained expertise in Java, 
             cloud computing, user interfaces and more.</p>
