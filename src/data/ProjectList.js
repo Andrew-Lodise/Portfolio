2d390febf725e3fb5d23c1d740d23b04f1b83508
Andrew-Lodise <AL964440@wcupa.edu>
2024-03-14 21:52:36 -0400

    put assets in src folder and imported them into js file

--- src/data/ProjectList.js
+++ src/data/ProjectList.js
@@ -1,7 +1,14 @@
+import wtw1 from "../assets/wtw1.JPG"
+import wtw2 from "../assets/wtw2.JPG"
+import homepage from "../assets/homepage.JPG"
+import skillspage from "../assets/skillspage.JPG"
+import pup1 from "../assets/pup1.PNG"
+import pup3 from "../assets/pup3.PNG"
+
 const ProjectList = [
   {
     title: 'Outfit Recommender',
-    images: ["/assets/wtw1.jpg", "/assets/wtw2.jpg"],
+    images: [wtw1, wtw2],
     tech: ['Python', 'Tkinter', 'REST api', 'Pandas'],
     description: `This app recommends outfits based on the weather data.
     The user enters the city they are in and then generates an outfit for 
@@ -14,7 +21,7 @@ const ProjectList = [
   },
   {
     title: 'Portfolio Website',
-    images: ["/assets/homepage.jpg", "/assets/skillspage.jpg"],
+    images: [homepage, skillspage],
     tech: ['React', 'Tailwind','Javascript'],
     description: `I made this website to show my future employers that I am determinded
     to enter the software development market and I'm not afraid to learn new technologies.
@@ -25,7 +32,7 @@ const ProjectList = [
   }, 
   {
     title: 'Stock Predictor CLI Tool',
-    images: ["/assets/pup1.png", "/assets/pup3.png"],
+    images: [pup3, pup1],
     tech: ['Python', 'Matplotlib','Pandas', 'Numpy'],
     description: `This app uses alpaca stock api to gather data of a given stock
     it then goes through multiple steps of data analysis to output information
