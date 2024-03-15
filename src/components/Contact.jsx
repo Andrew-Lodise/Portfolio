4bf9d37b8a9ae10a98fac20e88b701bbc01abfc2
Andrew-Lodise <AL964440@wcupa.edu>
2024-03-15 07:39:07 -0400

    form funtionality implemented

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,16 +16,17 @@ const Contact = () => {
 
       <div name="form div"
       className='w-full h-full flex flex-col items-center justify-center'>
-        <form action="POST" className='flex flex-col gap-y-4 text-form-input
-         text-gray-300 w-[80%] md:w-[60%] lg:w-[40%]'>
+        <form action="https://getform.io/f/eapwdyxb" method='POST' 
+        className='flex flex-col gap-y-4 text-form-inputtext-gray-300 
+        w-[80%] md:w-[60%] lg:w-[40%]'>
           <h1 className='text-head leading-none'>
             Connect with me!
           </h1> 
-          <input type="text" placeholder='Name' 
+          <input type="text" placeholder='Name' name="name" 
           className='bg-gray-900 border-[1px] border-gray-300 px-2 '/>
-          <input type="text" placeholder='Email' 
+          <input type="text" placeholder='Email' name="email" 
           className=' bg-gray-900 border-[1px] border-gray-300 px-2'/>
-          <textarea type="text" rows="5" placeholder='Message' 
+          <textarea type="text" rows="5" placeholder='Message' name="message" 
           className='bg-gray-900 border-[1px] border-gray-300 px-2'/>
           <button className='self-start border-[1px] border-gray-300 text-button
           px-2 hover:bg-gray-300 hover:text-gray-900 duration-200 flex items-center gap-2
@@ -33,7 +34,7 @@ const Contact = () => {
             <p>
               Submit
             </p>
-            <IoIosMail className='text-3xl group-hover:translate-x-10 duration-200'/>
+            <IoIosMail className='text-3xl group-hover:translate-x-10 duration-100'/>
           </button> 
         </form>
       </div>
